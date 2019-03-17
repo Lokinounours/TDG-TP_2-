938582db522a2f6f2f094c0de8415433014a0233
exob xD <pierre.woj@hotmail.fr>
2019-03-17 23:17:23 +0100

    menu

--- main.cpp
+++ main.cpp
@@ -3,13 +3,54 @@
 
 int main()
 {
+    graphe h{"graphe_1.txt"};
     graphe g{"graphe_2.txt"};
-    //std::cout << g.isEulerien();
-    //g.afficher();
-    //g.afficherBFS("1");
-    //g.recursifDFS("1");
-    g.afficherDFS("1");
-    //int ncc=g.rechercher_afficherToutesCC();
-    //std::cout << "Nombre de composantes connexes : " << ncc;
+    int choix = 0;
+    bool continuer = true;
+    std::string graph = "1";
+    while (continuer){
+        std::cout << "Choose what you want to do" << std::endl;
+        std::cout << "1-affichage BFS" << std::endl;
+        std::cout << "2-affichage DFS" << std::endl;
+        std::cout << "3-utiliser graph 1" << std::endl;
+        std::cout << "4-utiliser graph 2" << std::endl;
+        std::cout << "5-IsEulerien" << std::endl;
+        std::cout << "6-afficher composante connexe" << std::endl;
+        std::cin >> choix;
+        switch(choix){
+            case 1:
+                g.afficherBFS(graph);
+                break;
+            case 2:
+                g.afficherDFS(graph);
+                break;
+            case 3:
+                graph = "1";
+                break;
+            case 4:
+                graph = "2";
+                break;
+            case 5:
+                std::cout << g.isEulerien();
+                break;
+            case 6:
+                {
+                    int ncc=g.rechercher_afficherToutesCC();
+                    std::cout << "Nombre de composantes connexes : " << ncc;
+                    break;
+                }
+            default:
+                {
+                    std::cout << "Erreur" << std::endl;
+                    break;
+                }
+        }
+
+
+
+
+        std::cout << "Would you like to continue ?" << std::endl;
+        std::cin >> continuer;
+    }
     return 0;
 }
